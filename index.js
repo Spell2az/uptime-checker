626a4b52bb3ac9b4324ca5b9836adfebeb44f82a
Spell2az <martin_kovac@outlook.com>
2018-10-28 21:20:45 +0000

    Add basic routing

--- index.js
+++ index.js
@@ -1,11 +1,68 @@
-const http = require('http');
+const http = require("http");
+const url = require("url");
+const StringDecoder = require("string_decoder").StringDecoder;
 
 const port = 8080;
 
 const server = http.createServer((req, res) => {
-  res.end('Wassssup!');
+  const parsedUrl = url.parse(req.url, true);
+  const path = parsedUrl.pathname;
+  const trimmedPath = path.replace(/^\/+|\/+$/g, "");
+  const queryStringObject = parsedUrl.query;
+  const method = req.method;
+  const headers = req.headers;
+  const decoder = new StringDecoder("utf-8");
+
+  let buffer = "";
+
+  req.on("data", data => {
+    buffer += decoder.write(data);
+  });
+
+  req.on("end", () => {
+    buffer += decoder.end();
+
+    const choosenHandler =
+      typeof router[trimmedPath] !== "undefined"
+        ? router[trimmedPath]
+        : handlers.notFound;
+
+    const data = {
+      trimmedPath,
+      queryStringObject,
+      method,
+      headers,
+      payload: buffer
+    };
+
+    choosenHandler(data, (statusCode, payload) => {
+      statusCode = typeof statusCode === "number" ? statusCode : 200;
+
+      payload = typeof payload === "object" ? payload : {};
+
+      const payloadString = JSON.stringify(payload);
+
+      res.setHeader("Content-Type", "application/json");
+      res.writeHead(statusCode);
+      res.end(payloadString);
+      console.log(statusCode, payloadString);
+    });
+  });
 });
 
-server.listen(port, (err) => {
+server.listen(port, err => {
   console.log(`Server is listening on port: ${port}`);
-})
+});
+
+const handlers = {
+  sample(data, callback) {
+    callback(406, { name: "sample handler" });
+  },
+  notFound(data, callback) {
+    callback(404);
+  }
+};
+
+const router = {
+  sample: handlers.sample
+};
